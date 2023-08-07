4dc36c04f6857cdfcb3b36276250cafcf9b426aa
Noel-96 <noelorion69@gmail.com>
2023-08-07 14:29:06 +0100

    Initial Commit

---
+++ PitchFormationDemo/PitchFormationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchFormationDemoApp.swift
+//  PitchFormationDemo
+//
+//  Created by Noel Obaseki on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct PitchFormationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
